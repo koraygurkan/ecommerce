11cb8ed2ae42e191ff57f3602134d81ec31186c7
koraygurkan <koraygurkandanaci@gmail.com>
2025-07-30 23:37:11 +0300

    .

---
+++ resources/views/kategori.blade.php
@@ -0,0 +1,5 @@
+<a href="/kategori">Kategori</a>
+<a href="/urun">Ürün</a>
+<a href="/sepet">Sepet</a>
+<hr>
+<h1>Kategori</h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('kategori','kategori');
+Route::view('urun','urun');
+Route::view('sepet','sepet');
