622aa6f48af2372538912a9a4e5d1ee597afb793
koraygurkan <koraygurkandanaci@gmail.com>
2025-07-30 23:56:32 +0300

    .

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <a href="/">Ana Sayfa</a>
+    <a href="/kategori">Kategori</a>
+    <a href="/urun">Ürün</a>
+    <a href="/sepet">Sepet</a>
+    <hr>
+   @yield('content')
+    <hr>
+    E-Ticaret Sistemleri
+
+</body>
+</html>
