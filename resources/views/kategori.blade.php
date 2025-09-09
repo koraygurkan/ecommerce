fd7812bba7d5b08ef2870c591709304222eb1b6e
koraygurkan <koraygurkandanaci@gmail.com>
2025-09-09 22:00:59 +0300

    .

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -3,13 +3,3 @@
 @section('content')
     <h1>Kategori Sayfası</h1>
 @endsection
-
-
-<a href="/">Ana Sayfa</a>
-<a href="/kategori">Kategori</a>
-<a href="/urun">Ürün</a>
-<a href="/sepet">Sepet</a>
-
-
-<hr>
-E-Ticaret Sistemleri
--- resources/views/sepet.blade.php
+++ resources/views/sepet.blade.php
@@ -1,8 +1,5 @@
-<a href="/">Ana Sayfa</a>
-<a href="/kategori">Kategori</a>
-<a href="/urun">Ürün</a>
-<a href="/sepet">Sepet</a>
-<hr>
-<h1>Sepet</h1>
-<hr>
-E-Ticaret Sistemleri
+@extends('layouts.master')
+@section('title','Kategori')
+@section('content')
+    <h1>Sepet Sayfası</h1>
+@endsection
