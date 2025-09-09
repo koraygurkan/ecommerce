fc45507c3ddb212d04262b71e7a99eafc003ea6c
koraygurkan <koraygurkandanaci@gmail.com>
2025-09-09 22:30:58 +0300

    .

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,2 +1,11 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">Tüm Hakları Saklıdır &copy; 2025</div>
+            <div class="col-md-6 text-right"><a href="https://eticaret.koraygurkandanaci.com.tr">eticaret.koraygurkandanaci.com.tr</a></div>
+        </div>
+    </div>
+</footer>
+
 <hr>
-All Rights Reserved. © 2025 https://eticaret.koraygurkandanaci.com.tr/
+
